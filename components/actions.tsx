aedb7fb015db26bf137eb9fe77a315841530e378
taina <tainabugs13@gmail.com>
2024-05-17 13:52:50 -0300

    cards

--- components/actions.tsx
+++ components/actions.tsx
@@ -1,5 +1,11 @@
 "use client"
 
+import { DropdownMenuContentProps } from "@radix-ui/react-dropdown-menu";
+
 interface ActionProps {
-    
+    children: React.ReactNode;
+    side?: DropdownMenuContentProps["side"];
+    sideOffset?: DropdownMenuContentProps["sideOffset"];
+    id: string;
+    title: string;
 }
