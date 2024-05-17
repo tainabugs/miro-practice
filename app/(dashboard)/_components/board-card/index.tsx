80a055e4faeb5665f5d205035c6393f46b2860af
taina <tainabugs13@gmail.com>
2024-05-17 13:53:04 -0300

    c

--- app/(dashboard)/_components/board-card/index.tsx
+++ app/(dashboard)/_components/board-card/index.tsx
@@ -1,14 +1,16 @@
 "use client"
 
-import Image from "next/image";
 import Link from "next/link";
+import Image from "next/image";
+import { useAuth } from "@clerk/nextjs";
 import { formatDistanceToNow } from "date-fns";
 
+import { Actions } from "@/components/actions";
 import { Skeleton } from "@/components/ui/skeleton";
 
 import { Overlay } from "./overlay";
-import { useAuth } from "@clerk/nextjs";
 import { Footer } from "./footer";
+import { MoreHorizontal } from "lucide-react";
 
 interface BoardCardProps {
     id: string;
@@ -49,6 +51,19 @@ export const BoardCard = ({
                         className="object-fit"
                     />
                     <Overlay />
+                    <Actions 
+                    id={id}
+                    title={title}
+                    side="right"
+                    >
+                        <button
+                        className="absolute top-1 right-1 opacity-0 group-hover:opacity-100 transition-opacity px-3 py-2 outline-none"
+                        >
+                            <MoreHorizontal 
+                            className="text-white opacity-75 hover:opacity-100 transition-opacity"
+                            />
+                        </button>
+                    </Actions>
                 </div>
                 <Footer 
                     isFavorite={isFavorite}
